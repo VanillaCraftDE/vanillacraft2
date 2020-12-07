aceea1ae266bbdc551fbd9a72b0e37bb2277edce
Laurino108 <72888948+Laurino108@users.noreply.github.com>
2020-12-07 15:41:08 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+
+/*******************************************************
+ * Copyright (C) 2020 Nicolas Proske (AKA pume)
+ * Twitter: https://twitter.com/einpume
+ *
+ * This file is part of "PORTAL!".
+ *
+ * "PORTAL!" can not be distributed without
+ * the express permission of Nicolas Proske (AKA pume)
+ *
+ * This copyright notice may not be removed or changed.
+ *******************************************************/
+
+/* SERVER INFO */
+$server_ip = 'YourServer.net';
+
+/* ICON LINKS */
+$forum_link = 'https://www.yourserver.net/forum';
+$vote_link = 'https://www.yourserver.net/vote';
+$shop_link = 'https://www.yourserver.net/shop';
+
+/* HEADER TEXT */
+$header_title = 'YourServer.net';
+$header_caption = 'Welcome to our "PORTAL!" website';
+
+/* SOCIAL MEDIA LINKS */
+$twitter_link = 'https://twitter.com/einpume';
+$teamspeak_ip = 'YourServer.net';
+$discord_link = 'http://discord.gg/s4vQkQy';
+
+/* SERVER INFO TEXT */
+$server_info_loading = 'Loading server data.. ';
+$server_info_server_offline = 'Server is offline';
+
+/* FOOTER TEXT */
+$footer_imprint_link = 'https://yourserver.net/imprint';
+$footer_imprint_text = 'Imprint & Privacy Policy';
